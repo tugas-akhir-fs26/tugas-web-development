1e1939b306647df670c255d96c9447270cb1825f
John Doe <andridaniata@gmail.com>
2023-10-11 22:32:45 +0700

    ambil value dari user

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Perubahan");
+let login = document.getElementById("login");
+
+login.addEventListener("click", function () {
+  let email_login = document.getElementById("email_login").value;
+  let password_login = document.getElementById("password_login").value;
+
+  console.log(email_login);
+  console.log(password_login);
+});
