adb9793550b35a1b7e0126e6b7e5e3802c88ee3a
Dionisius951 <dionisius951@gmail.com>
2023-10-18 23:41:06 +0700

    memperbaiki logic login

--- script.js
+++
@@ -1,9 +0,0 @@
-const nonMember = document.getElementById('non-member');
-
-console.log(nonMember);
-const isLogin = localStorage.getItem("email")
-if (isLogin){
-    nonMember.style.display = "none"
-} else {
-    nonMember.style.display = "block"
-}
